d1fbc531c203ac19c832869dc7727cff7d41148c
RUIZROCK <sergioruizrock@gmail.com>
2022-09-07 17:36:59 -0300

    TP realizado hasta  el punto 3

--- tp1/Program.cs
+++ tp1/Program.cs
@@ -11,74 +11,201 @@ namespace tp1
     {
         static void Main(string[] args)
         {
+            ////punto 2
+            ////problema 1
+            //Console.WriteLine("problema 1");
+            //try
+            //{
+            //    Console.WriteLine("Ingrese un numero");
+            //    int texto = Convert.ToInt32(Console.ReadLine());
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Error");
+            //}
 
-            //punto 1
-            Console.WriteLine("punto 1");
-            try
-            {
-                Console.WriteLine("Ingrese un numero");
-                int texto = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Error");
-            }
+            ////problema 2
+            //Console.WriteLine("\n\nproblema 2");
+            //try
+            //{
+            //    Console.WriteLine("Ingrese un numero.");
+            //    float dividendo = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Ingrese otro numero.");
+            //    float divisor = Convert.ToInt32(Console.ReadLine());
+            //    float total = dividendo / divisor;
+            //    Console.WriteLine("Total " + total);
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("error");
+            //}
 
-            //punto 2
-            Console.WriteLine("\n\npunto 2");
-            try
-            {
-                Console.WriteLine("Ingrese un numero.");
-                float dividendo = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Ingrese otro numero.");
-                float divisor = Convert.ToInt32(Console.ReadLine());
-                float total = dividendo / divisor;
-                Console.WriteLine("Total " + total);
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("error");
-            }
+            ////problema 3
+            //Console.WriteLine("\n\nproblema 3");
+            //try
+            //{
+            //    Console.WriteLine("Ingrese kilometros conducidos.");
+            //    float kilometros = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Ingrese litros usados.");
+            //    float litros = Convert.ToInt32(Console.ReadLine());
+            //    float total = kilometros / litros;
+
+            //    Console.WriteLine("Total " + total);
+            //}
+            //catch(DivideByZeroException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("error en los datos");
+
+            //}
+
+            ////problema 4
+            //Console.WriteLine("\n\nproblema 4");
+            //Console.WriteLine("Lista de provincias obtenidas por una api");
+            //try
+            //{
+            //    List<Provincia> listaProvincias = apiJson();
+
+            //    foreach (var item in listaProvincias)
+            //    {
+            //        Console.WriteLine("id: {0} - nombre: {1}", item.Id, item.Nombre);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
 
             //punto 3
-            Console.WriteLine("\n\npunto 3");
+            Console.WriteLine("\n\n\npunto  3");
             try
             {
-                Console.WriteLine("Ingrese kilometros conducidos.");
-                float kilometros = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Ingrese litros usados.");
-                float litros = Convert.ToInt32(Console.ReadLine());
-                float total = kilometros / litros;
+                Console.WriteLine("Datos personales,profesionales y laborales del empleado:");
+                Console.WriteLine("1-Nombre");
+                string nombre = Convert.ToString(Console.ReadLine());
 
-                Console.WriteLine("Total " + total);
-            }
-            catch(DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("error en los datos");
+                Console.WriteLine("2-Apellido");
+                string apellido = Convert.ToString(Console.ReadLine());
 
-            }
+                Console.WriteLine("3-fecha de nacimiento");
+                DateTime fechaNac = Convert.ToDateTime(Console.ReadLine());
 
-            //punto 4
-            Console.WriteLine("\n\npunto 4");
-            Console.WriteLine("Lista de provincias obtenidas por una api");
-            try
-            {
-                List<Provincia> listaProvincias = apiJson();
+                Console.WriteLine("4-dni");
+                int dni = Convert.ToInt32(Console.ReadLine());
 
-                foreach (var item in listaProvincias)
+                Console.WriteLine("5-Direccion");
+                string direccion = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("6-telefono");
+                string telefono = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("7-casado (responda con si o no)");
+                string respuesta = Convert.ToString(Console.ReadLine());
+                int cantHijo = 0;
+                bool casado;
+                bool divorciado = false;
+                DateTime fechaDivorcio = DateTime.MinValue;
+                if (respuesta == "si")
                 {
-                    Console.WriteLine("id: {0} - nombre: {1}", item.Id, item.Nombre);
+                    casado = true;
+
+                    Console.WriteLine("8-cantidad de hijos (en numeros)");
+                    cantHijo = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("9-divorciado");
+                    respuesta = Convert.ToString(Console.ReadLine());
+
+                    if (respuesta == "si")
+                    {
+                        divorciado = true;
+                        Console.WriteLine("10-fecha de divorcio");
+                        fechaDivorcio = Convert.ToDateTime(Console.ReadLine());
+                    }
                 }
+                else
+                {
+                    casado = false;
+                    divorciado = false;
+                }
+
+
+
+                Console.WriteLine("11-titulo universitario");
+                respuesta = Convert.ToString(Console.ReadLine());
+                bool tituloUniversitario;
+                string nombreTituloUniversitario;
+                string universidad;
+                if (respuesta == "si")
+                {
+                    tituloUniversitario = true;
+
+                    Console.WriteLine("12-nombre del titulo universitario");
+                    nombreTituloUniversitario = Convert.ToString(Console.ReadLine());
+
+                    Console.WriteLine("13-universidad");
+                    universidad = Convert.ToString(Console.ReadLine());
+                }
+                else
+                {
+                    tituloUniversitario = false;
+                    nombreTituloUniversitario = String.Empty;
+                    universidad = String.Empty;
+                }
+
+                Console.WriteLine("14-fecha de ingreso al trabajo");
+                DateTime fechaIngresoTrabajo = Convert.ToDateTime(Console.ReadLine());
+
+                Console.WriteLine("15-sueldo");
+                double sueldo = Convert.ToDouble(Console.ReadLine());
+
+                empleado trabajador = new empleado(
+                    nombre, apellido, direccion,
+                    nombreTituloUniversitario,
+                    universidad, dni, telefono, cantHijo,
+                    sueldo, fechaNac, fechaIngresoTrabajo,
+                    fechaDivorcio, casado, divorciado, tituloUniversitario
+                    );
+
+                double antiguedad = trabajador.calcularAntiguedad();
+                int edad = trabajador.calcularEdad();
+                double salario = trabajador.CalcularSalario();
+                string estadCivil = trabajador.Casado ? "Casado" : "soltero";
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Resultados");
+                Console.WriteLine("Datos del empleado:");
+                Console.WriteLine("");
+                Console.WriteLine("Nombre : "+ trabajador.Nombre+"  Apellido : "+trabajador.Apellido);
+                Console.WriteLine("Fecha de nacimiento : "+trabajador.Fecha_nacimiento + "  Edad : "+edad);
+                Console.WriteLine("DNI : "+trabajador.Dni+"  Direccion : "+trabajador.Direccion);
+                Console.WriteLine("Telefono : "+trabajador.Telefono + "  Estado civil : "+ estadCivil);
+                if (estadCivil=="casado" && trabajador.Divorciado==true)
+                {
+                    Console.WriteLine("Cantidad de hijos : "+trabajador.CantidadHijos);
+                    Console.WriteLine("Divorciado : SI  Fecha de divorcio : "+trabajador.Fecha_divorcio);
+                }
+
+                if (trabajador.TituloUniversitario == true)
+                {
+                    Console.WriteLine("Profesion : "+ trabajador.Nombre_titulo_universitario+ "  Universidad : "+ trabajador.Universidad);
+                }
+                else
+                {
+                    Console.WriteLine("Profesion : NO");
+                }
+                Console.WriteLine("Fecha de ingreso a la empresa : "+trabajador.Fecha_ingreso);
+                Console.WriteLine("antiguedad del empleado"+ antiguedad);
+                Console.WriteLine("sueldo: "+ salario);
+
             }
-            catch (Exception e)
+            catch(Exception e)
             {
                 Console.WriteLine(e.Message);
             }
---
+++ tp1/empleado.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp1
+{
+    class empleado
+    {
+        private static float descuento = 0.15f;
+
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private string nombre_titulo_universitario;
+        private string universidad;
+
+        private int dni;
+        private string telefono;
+        private int cantidadHijos;
+
+        private double sueldo;
+
+        private DateTime fecha_nacimiento;
+        private DateTime fecha_ingreso;
+        private DateTime fecha_divorcio;
+
+        private bool casado;
+        private bool divorciado;
+        private bool tituloUniversitario;
+
+        public empleado(string nombre, 
+            string apellido, 
+            string direccion, 
+            string nombre_titulo_universitario, 
+            string universidad, 
+            int dni, 
+            string telefono, 
+            int cantidadHijos, 
+            double sueldo, 
+            DateTime fecha_nacimiento, 
+            DateTime fecha_ingreso, 
+            DateTime fecha_divorcio, 
+            bool casado, 
+            bool divorciado, 
+            bool tituloUniversitario)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.nombre_titulo_universitario = nombre_titulo_universitario;
+            this.universidad = universidad;
+            this.dni = dni;
+            this.telefono = telefono;
+            this.cantidadHijos = cantidadHijos;
+            this.sueldo = sueldo;
+            this.fecha_nacimiento = fecha_nacimiento;
+            this.fecha_ingreso = fecha_ingreso;
+            this.fecha_divorcio = fecha_divorcio;
+            this.casado = casado;
+            this.divorciado = divorciado;
+            this.tituloUniversitario = tituloUniversitario;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Nombre_titulo_universitario { get => nombre_titulo_universitario; set => nombre_titulo_universitario = value; }
+        public string Universidad { get => universidad; set => universidad = value; }
+        public int Dni { get => dni; set => dni = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public int CantidadHijos { get => cantidadHijos; set => cantidadHijos = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+        public DateTime Fecha_nacimiento { get => fecha_nacimiento; set => fecha_nacimiento = value; }
+        public DateTime Fecha_ingreso { get => fecha_ingreso; set => fecha_ingreso = value; }
+        public DateTime Fecha_divorcio { get => fecha_divorcio; set => fecha_divorcio = value; }
+        public bool Casado { get => casado; set => casado = value; }
+        public bool Divorciado { get => divorciado; set => divorciado = value; }
+        public bool TituloUniversitario { get => tituloUniversitario; set => tituloUniversitario = value; }
+
+        public int calcularAntiguedad()
+        {
+            try
+            {
+                TimeSpan spanAntiguedad = DiasEntreFechas(DateTime.Now, Fecha_ingreso);
+                int antiguedadEnDias = (int)spanAntiguedad.TotalDays;
+                return antiguedadEnDias / 365;
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+        }
+
+        public int calcularEdad()
+        {
+            try
+            {
+                TimeSpan spanEdad = DiasEntreFechas(DateTime.Now, Fecha_nacimiento);
+                return (int)spanEdad.TotalDays / 365;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+        }
+
+        public double CalcularSalario()
+        {
+            try
+            {
+                double Adicional = Sueldo * CalcularAdicional();
+                double Descuento = Sueldo * empleado.descuento;
+                double salarioFinal = Sueldo + Adicional - Descuento;
+                return salarioFinal;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0; 
+        }
+         
+        double CalcularAdicional()
+        {
+            try
+            {
+                int AniosAntiguedad = calcularAntiguedad();
+                if (AniosAntiguedad < 20)
+                {
+                    return AniosAntiguedad * 0.01;
+                }
+                else
+                {
+                    return 0.25;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+        }
+
+        static TimeSpan DiasEntreFechas(DateTime f1, DateTime f2)
+        {
+            TimeSpan timespan = f1.Subtract(f2);
+            return timespan;
+        }
+    }
+}
